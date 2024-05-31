cc28c09cd556c602e4a8c34549f2243d41f57459
Jaimeangel <jaimeangel1097@gmail.com>
2024-05-31 17:44:06 -0500

    creando seralizers para productos/stocks y vistas DRF

--- SIP/stock_product/models.py
+++ SIP/stock_product/models.py
@@ -4,13 +4,28 @@ class Category(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
 
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price_sale = models.DecimalField(max_digits=10, decimal_places=2)
 
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        # Check if the Stock instance exists, if not, create it
+        if not hasattr(self, 'stock'):
+            Stock.objects.create(product=self, current_stock=0)
+
+    def __str__(self):
+        return self.name
+
 class Stock(models.Model):
     product = models.OneToOneField(Product, on_delete=models.CASCADE, primary_key=True)
     current_stock = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.product.name
---
+++ SIP/stock_product/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import Category,Product,Stock
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'  # _all_ para obtener todos lo datos
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'  # _all_ para obtener todos lo datos
+
+class StockSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Stock
+        fields = '__all__'  # _all_ para obtener todos lo datos

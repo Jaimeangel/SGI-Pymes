1dfecbe9b5bb4033c02ae0731a72b79eeeec0a65
Jaime A <jaimeangel1097@gmail.com>
2024-06-16 15:14:49 -0500

    agregando modelos a admin

--- SIP/order_sale/admin.py
+++ SIP/order_sale/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import OrderSale,OrderSaleDetail
+
+admin.site.register(OrderSale)
+admin.site.register(OrderSaleDetail)
--- SIP/order_supplier/admin.py
+++ SIP/order_supplier/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import OrderSupplier,OrderSupplierDetail
+
+admin.site.register(OrderSupplier)
+admin.site.register(OrderSupplierDetail)
